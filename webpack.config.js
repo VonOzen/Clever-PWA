bddba0f1f04cdbd10538088510e3049580313dae
Simon Escure <sescure@clever-age.com>
2019-02-14 14:06:28 +0100

    initial commit - webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+//const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    mode: 'development',
+    entry: {
+        app: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: __dirname + '/build',
+        filename: '[name].js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '/build'),
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+};
+
+module.exports = config;
