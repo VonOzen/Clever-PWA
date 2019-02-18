7df24ee5a49ab7529faa53551a24cd524b51b8f5
Simon Escure <sescure@clever-age.com>
2019-02-18 13:02:03 +0100

    updated eslint config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import 'reset-css/sass/_reset.scss';
 
-import App from './app/components/App'
+import App from './app/components/App';
 
 ReactDom.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,51 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-    mode: 'development',
-    entry: {
-        app: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: __dirname + '/build',
-        filename: '[name].js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, '/build'),
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: {
-                    loader: 'babel-loader'
-                },
-                exclude: /node_modules/
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use : [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        })
-    ]
+	mode: 'development',
+	entry: {
+		app: path.resolve(__dirname, 'src/index.js')
+	},
+	output: {
+		path: __dirname + '/build',
+		filename: '[name].js'
+	},
+	devServer: {
+		contentBase: path.join(__dirname, '/build'),
+		historyApiFallback: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: {
+					loader: 'babel-loader'
+				},
+				exclude: /node_modules/
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use : [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html'
+		})
+	]
 };
 
 module.exports = config;
